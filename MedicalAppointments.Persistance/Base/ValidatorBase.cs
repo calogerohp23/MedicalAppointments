7f017ede49d77d73cd0cfd846d246c2f948872dd
Calogero Henríquez <calogero.henper@gmail.com>
2024-10-19 01:39:26 -0400

    Persistence layer modifications

---
+++ MedicalAppointments.Persistance/Base/ValidatorBase.cs
@@ -0,0 +1,23 @@
+﻿using MedicalAppointments.Domain.Result;
+
+namespace MedicalAppointments.Persistance.Base
+{
+    public abstract class ValidatorBase<TEntity> where TEntity: class
+    {
+        public virtual OperationResult Validate(TEntity entity) 
+        {
+            OperationResult operationResult = new OperationResult();
+
+            if (entity == null) 
+            {
+                operationResult.Success = false;
+                operationResult.Message = "The entity is null";
+                return operationResult;
+            }
+            if (entity == 0)
+            {
+
+            }
+        }
+    }
+}
--- MedicalAppointments.Persistance/Repositories/Users/UsersRepository.cs
+++ MedicalAppointments.Persistance/Repositories/Users/UsersRepository.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MedicalAppointments.Domain.Entities.Appointments;
+using MedicalAppointments.Domain.Result;
+using MedicalAppointments.Persistance.Base;
+using MedicalAppointments.Persistance.Context;
+using MedicalAppointments.Persistance.Interfaces.Users;
+using MedicalAppointments.Persistance.Repositories.Appointments;
+using Microsoft.Extensions.Logging;
+using System.Linq.Expressions;
 
 namespace MedicalAppointments.Persistance.Repositories.Users
 {
-    internal class UsersRepository
+    public class UsersRepository : BaseRepository<Appointment>, IUsersRepository
     {
+        private readonly MedicalAppointmentContext _medicalAppointmentContext;
+        private readonly ILogger<AppointmentRepository> logger;
+        public UsersRepository(MedicalAppointmentContext medicalAppointmentContext, ILogger<AppointmentRepository> logger) : base(medicalAppointmentContext)
+        {
+            _medicalAppointmentContext = medicalAppointmentContext;
+            this.logger = logger;
+        }
+        public Task<OperationResult> Save(Domain.Entities.Users.Users entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> Update(Domain.Entities.Users.Users entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> Remove(Domain.Entities.Users.Users entity)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        }
+
+        public Task<bool> Exists(Expression<Func<Domain.Entities.Users.Users, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
