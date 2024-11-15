038a796140901fe3e07f80815049c83ff1070600
calogerohp23 <calogero.henper@gmail.com>
2024-11-15 12:20:04 -0400

    Adding folders and first user dependency.

---
+++ MedicalAppointment.IOC/Dependencies/Users/UsersDependency.cs
@@ -0,0 +1,16 @@
+﻿using MedicalAppointments.Persistance.Interfaces.Users;
+using MedicalAppointments.Persistance.Repositories.Users;
+using Microsoft.Extensions.DependencyInjection;
+namespace MedicalAppointment.IOC.Dependencies.Users
+{
+    public static class UsersDependency
+    {
+        public static void AddUsersDependency(this IServiceCollection service)
+        {
+            service.AddScoped<IDoctorsRepository, DoctorsRepository>();
+            service.AddScoped<IPatientsRepository, PatientsRepository>();
+            service.AddScoped<IUsersRepository, UsersRepository>();
+            //service.AddTransient<>
+        }
+    }
+}
